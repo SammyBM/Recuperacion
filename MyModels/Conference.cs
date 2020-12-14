bce628301a9c4256fdd16379ab88575601126115
SammyBM <samuelbarraganmiranda@gmail.com>
2020-12-14 10:09:17 -0600

    Presenter

--- MyModels/Conference.cs
+++ MyModels/Conference.cs
@@ -7,7 +7,7 @@ namespace Recuperacion.MyModels
         [key]
         public int ConferenceID { get; set; }
         public string Name { get; set; }
-        public Person Presenter { get; set; }
+        public int Presenter { get; set; }
         public DateTime Begin { get; set; }
         public DateTime End { get; set; }
         public decimal? Cost {get; set; }
